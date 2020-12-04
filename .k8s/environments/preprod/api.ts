9f9684949b745a83bc6dbecfbf61e5386f693769
Douglas DUTEIL <douglasduteil@gmail.com>
2020-12-04 18:24:33 +0100

    feat(k8s): use @socialgouv/kosko-charts@alpha

--- .k8s/environments/preprod/api.ts
+++
@@ -1,7 +0,0 @@
-import { AppConfig } from "@socialgouv/kosko-charts/components/app";
-import { ok } from "assert";
-ok(process.env.CI_PROJECT_NAME);
-ok(process.env.KUBE_NAMESPACE);
-export default {
-  subdomain: `preprod-${process.env.CI_PROJECT_NAME}`,
-} as Partial<AppConfig>;
