40a91391de8b22cbab9a99d032f114cd078d9904
Douglas Duteil <douglasduteil@gmail.com>
2021-03-23 16:19:52 +0100

    feat(k8s): bring back the hpa (#83)
    Revert "feat(k8s): remove hpa (#54)"

--- .k8s/components/serving-ml.ts
+++ .k8s/components/serving-ml.ts
@@ -1,5 +1,8 @@
+import { ok } from "assert";
 import env from "@kosko/env";
 import { create } from "@socialgouv/kosko-charts/components/app";
+import type{ Deployment } from "kubernetes-models/apps/v1";
+import { HorizontalPodAutoscaler } from "kubernetes-models/autoscaling/v2beta2";
 import { IIoK8sApiCoreV1HTTPGetAction } from "kubernetes-models/v1";
 import { getHarborImagePath } from "@socialgouv/kosko-charts/utils/getHarborImagePath";
 
@@ -43,5 +46,47 @@ const manifests = create("serving-ml", {
     },
   },
 });
+const deployment = manifests.find(
+  (manifest): manifest is Deployment => manifest.kind === "Deployment"
+);
+ok(deployment);
 
-export default [...manifests];
+
+const hpa = new HorizontalPodAutoscaler({
+  metadata: deployment.metadata,
+  spec: {
+    minReplicas: process.env.CI_COMMIT_TAG ? 2 : 1,
+    maxReplicas: 10,
+
+    metrics: [
+      {
+        resource: {
+          name: "cpu",
+          target: {
+            averageUtilization: 4000,
+            type: "Utilization",
+          },
+        },
+        type: "Resource",
+      },
+      {
+        resource: {
+          name: "memory",
+          target: {
+            averageUtilization: 100,
+            type: "Utilization",
+          },
+        },
+        type: "Resource",
+      },
+    ],
+
+    scaleTargetRef: {
+      apiVersion: deployment.apiVersion,
+      kind: deployment.kind,
+      name: deployment.metadata!.name!,
+    },
+  },
+});
+
+export default [...manifests, hpa];
